54c408ba7e8f739327c7e726d9b42e4ce89bdc5c
Matheus Pereira <matheuzpereiraa@gmail.com>
2024-10-17 17:28:42 -0300

    2° commit - aula de operadores logicos

---
+++ seção 3 - Logica de programacao/OPERADORES LOGICOS/script.js
@@ -0,0 +1,15 @@
+/*
+OPERADORES LOGICOS
+
+&& -> AND -> E      = é verdadeiro quando todas forem verdadeiro
+|| -> OR -> OU      = só é falsa quando todas forem falsa
+! -> NOT -> NEGAÇÃO = o que é vdd vira falso e o que é falso vira vdd
+
+*/
+
+console.log(true || true); //TRUE, pq para ser falso precisa todos serem falso.
+
+console.log(true && false); //FALSE, pq para ser VERDADEIRO precisa todos serem TRUE.
+
+console.log(!false);        //o que é vdd vira falso e o que é falso vira vdd.
+
